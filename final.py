17c109f534953a0ccd4c90931a8397c7c0005932
Minmyatngwe <139677126+Minmyatngwe@users.noreply.github.com>
2025-03-27 15:52:25 +0200

    Update final.py

--- final.py
+++ final.py
@@ -76,15 +76,19 @@ if st.button("Show recommendation"):
         overview=np.array([get_overview(selected_movies[0])])
         st.text(selected_movies[0])
         st.write(f"""
-        <div style="display: flex;">
-        <div style="width:500px">
-            <img src={selected_image[0]} alt="Image" style="width: 100%; height: auto;";/>
-        </div>
-        <div style="padding-left: 50px; width=200px">
-            <h1>OverView</h1>
-            <p1>Release Date-{overview[0][2]}</p1>
-        </div>
-        </div>""", unsafe_allow_html=True)
+                <div style="display: flex; width: 100%;">
+                    <div style="width: 500px; flex-shrink: 0;">
+                        <img src={selected_image[0]} alt="Image" style="width: 100%; height: auto;"/>
+                    </div>
+                    <div style="padding-left: 50px; width: 200px;">
+                        <h1>OverView</h1>
+                        <p>Release Date-{overview[0][2]}</p>
+                        <p>Run Time: {overview[0][1]} min</p>
+                        <p>Production Country: {overview[0][3]}</p>
+                        <p>Rating: {overview[0][4]}</p>
+                    </div>
+                </div>
+                """, unsafe_allow_html=True)
 
         st.text('Released Date-' + overview[0][0])
         st.text('Run Time ' + overview[0][1]+ 'min ')
